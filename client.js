78ae85a6677ac72a4d5c16d4d858b3dc6eab6b1f
Nick <winslowknick@gmail.com>
2024-03-26 22:26:43 -0500

    added function with conditional statements. provided a loop as well.

--- client.js
+++ client.js
@@ -48,10 +48,65 @@ console.log('array of employee data: ',  employees );
 
 // This function will calculate 1 employee's bonus!
 //
-function calculateIndividualEmployeeBonus( employee ) {  
-  // your logic here
-  
-  
-  // return new object with bonus results
+function calculateIndividualEmployeeBonus( employee ) { 
+let bonusPercentage; //local varibale within the funciton
 
+//Those who have a rating of a 2 or below should not receive a bonus.
+//Those who have a rating of a 3 should receive a base bonus of 4% of their base annual income.
+//Those who have a rating of a 4 should receive a base bonus of 6% of their base annual income.
+//Those who have a rating of a 5 should receive a base bonus of 10% of their base annual income.
+  if (employee.reviewRating <= 2) {
+    bonusPercentage = 0;
+  } else if (employee.reviewRating === 3) {
+    bonusPercentage = .04;
+  } else if (employee.reviewRating === 4) {
+    bonusPercentage = .06;
+  } else if ( employee.reviewRating === 5) {
+    bonusPercentage = .01;
+  }
+  
+// checking if the employee has 4 digits to their employee number to add 5% to their bonus
+if (employee.employeeNumber.length === 4) {
+  bonusPercentage += .05;
 }
+
+// If employee's annualSalary > 65000, subtract 1% from bonusPercentage:
+//Number() converts a string number to a number value or non string number
+if (Number(employee.annualSalary) > 65000) {
+  bonusPercentage -= .01;
+}
+
+
+// Make sure employee bonuspercentage is between 0 and 13%
+if (bonusPercentage > .13) {
+  bonusPercentage = .13;
+} else if (bonusPercentage < 0) {
+  bonusPercentage = 0;
+}
+
+// calculate totalBonus
+let totalBonus = Number(employee.annualSalary) * bonusPercentage;
+
+// calculate totalCompensation
+let totalCompensation = Number(employee.annualSalary) + totalBonus;
+
+let runEachEmployee = {
+  name: employee.name,
+  bonusPercentage: bonusPercentage,
+  totalCompensation: totalCompensation,
+  totalBonus: totalBonus
+}
+
+// return new object with bonus results
+return runEachEmployee;
+
+
+};
+
+for (let i = 0; i < employees.length; i++) { // loop through array called employees
+  let result = calculateIndividualEmployeeBonus(employees[i]); //varibale that = runnning the function through each employee
+   console.log(result); // log the result 
+}
+
+
+
